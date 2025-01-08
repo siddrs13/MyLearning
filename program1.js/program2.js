97b31f044634c489805b4deeac91e89220c393dd
Siddharth Rs <siddharthrs@Siddharths-MacBook-Pro.local>
2025-01-08 12:20:02 +0530

    check

--- program1.js/program2.js
+++ program1.js/program2.js
@@ -1,12 +1,1 @@
-console.log('Hello World')
-let person = {name : 'siddharth',
-    age  : 28
-}
-let check = person.age
-console.log(check)
-console.log(person['name'])
-function addNumbers(a,b)
-{return a+b}
-addNumbers(6,6)
-addNumbers(6,6)
-addNumbers(6,6)
+check
